472892efd12d5777315b8e60922b1ec1b4df3f0a
Amovane <amovane@163.com>
2023-10-06 18:25:02 +0800

    optimize: add MAX_PAGE_QUERIES

--- x/followings.go
+++ x/followings.go
@@ -4,6 +4,8 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+const MAX_PAGE_QUERIES = 10
+
 func (x *X) GetFollowingsByScreenName(user string, cursor *string) (resp []UserResults, nextCursor *string, err error) {
 	uid, _ := x.scraper.GetUserIDByScreenName(user)
 	return x.GetRelationsById(uid, cursor, Following)
@@ -16,6 +18,7 @@ func (x *X) GetFollowingsById(uid string, cursor *string) (resp []UserResults, n
 func (x *X) IsFollowing(uid string, uidOfFollower string) bool {
 	var err error
 	var cursor *string
+	page := 0
 	for {
 		var pagedUsers []UserResults
 		pagedUsers, cursor, err = x.GetFollowingsById(uidOfFollower, cursor)
@@ -23,9 +26,10 @@ func (x *X) IsFollowing(uid string, uidOfFollower string) bool {
 		if slices.Contains(ids, uid) {
 			return true
 		}
-		if cursor == nil || err != nil {
+		if cursor == nil || err != nil || page >= MAX_PAGE_QUERIES {
 			break
 		}
+		page++
 	}
 	return false
 }
